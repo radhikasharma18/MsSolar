b731109dbb069f64b978d70d940ba3dad806e8ea
Radhika <sharmaradhika1803@gmail.com>
2025-11-17 18:01:05 +0530

    solar commit

---
+++ postcss.config.ts
@@ -0,0 +1,5 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},  // Tailwind v4 plugin
+  },
+};
---
+++ src/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <section>
+            <div className='bg-black h-screen flex flex-col justify-between p-16 text-white'>
+                <div>
+                <div>
+                <div>MS Solar</div>
+                <div></div>
+                <div></div>
+                </div>
+                <div></div>
+                </div>
+                <div>2</div>
+
+
+
+            </div>
+        </section>
+
+
+
+    </div>
+  )
+}
+
+export default Home
